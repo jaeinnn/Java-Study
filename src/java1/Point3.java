84a587463d9596ff0d77e6d7b945e9354fbf47bc
jaeout <jaeout2@gmail.com>
2022-03-17 18:28:47 +0900

    chap6.예제 6-2

---
+++ src/java1/Point3.java
@@ -0,0 +1,13 @@
+package java1;
+
+ class Point3 {
+    int x,y;
+    public Point3(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString(){
+        return "Point(" + x + "," + y + ")";
+    }
+}
---
+++ src/java1/ToStringEx.java
@@ -0,0 +1,10 @@
+package java1;
+
+public class ToStringEx {
+    public static void main(String[] args) {
+        Point3 p = new Point3(2,3);
+        System.out.println(p.toString());
+        System.out.println(p);
+        System.out.println(p + "입니다.");
+    }
+}
